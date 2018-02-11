5c1347feb2e74d58b06abf98a7fc396a93f899dc
rowan <rowanlupton@gmail.com>
2018-02-11 08:47:37 -0500

    rewrite in css grid, high memory usage during image resize due to header height

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,39 +1,44 @@
 function initialSize () {
-	var headerHeight = document.getElementById('header').clientHeight;
+	var headerHeight = document.querySelector('header').clientHeight;
 	var windowHeight = window.innerHeight;
 	var oneVW = 0.01 * window.innerWidth;
 	var oneVH = 0.01 * window.innerHeight;
 
-	document.getElementById('headerWrapper').style.height = headerHeight + 'px';
-	document.querySelector('.section#home>.row').style.minHeight = windowHeight - headerHeight + 'px';
+	document.querySelector('section#home').style.minHeight = windowHeight - headerHeight + 'px';
 
-	var newHeaderHeight = document.getElementById('topHeader').clientHeight;
+	var newHeaderHeight = document.getElementById('topHeader').clientHeight + 10 * oneVW;
 	var sectionHeight = window.innerHeight - newHeaderHeight;
-	document.querySelector('section.full-height').style.minHeight = sectionHeight;
-}; window.addEventListener('load', initialSize()); window.addEventListener('resize', initialSize());
+	fullHeightSections = document.querySelectorAll('section.full-height')
+	for (var i = 0; i < fullHeightSections.length; i++) {
+		fullHeightSections[i].style.minHeight = sectionHeight + 'px';
+	}
+}; window.addEventListener('load', initialSize());
 
 function scroll () {
+	var img = document.getElementById('header-image');
+	var currentHeight = img.clientHeight + 'px';
 	var currentScroll = window.scrollY;
-	var currentHeight = document.querySelector('.header>.img-container').clientHeight + 'px';
 	var oneVW = 0.01 * window.innerWidth;
 
 	var breakPoint = 20 * oneVW;
 
 	if (currentScroll < 1) {
-		maxHeight = 30 + 'vw';
+		maxHeight = 30 * oneVW + 'px';
 	} else if (currentScroll < breakPoint) {
 		maxHeight = 30 * oneVW - currentScroll + 'px';
-		
 	} else {
-		maxHeight = 10 + 'vw';
+		maxHeight = 10 * oneVW + 'px';
 	};
-	var img = document.querySelector('.header>.img-container');
+	
+	// img.style.height = maxHeight;
 	img.animate({height: [currentHeight, maxHeight]},
-				 {fill: 'forwards'})
+				 {fill: 'forwards'});
+	// console.log(maxHeight.replace('px', '').replace('vw', ''))
+	// document.querySelector('header').style.height = parseInt(maxHeight.replace('px', '')) + document.querySelector('header>#topHeader').clientHeight + 'px';
 
 } window.addEventListener('scroll', scroll);
 
-// side button scolling on mobile
+// nav button scrolling
 function navbarScroll (event) {
 	var href = event.target.getAttribute('href').substr(1);
 	if (href.length) {
@@ -41,9 +46,10 @@ function navbarScroll (event) {
 		var oneVW = 0.01 * window.innerWidth;
 
 		if (href == 'home') {
-			var currentHeight = document.querySelector('.header>.img-container').clientHeight + 'px';
+			var img = document.querySelector('header>.img-container');
+			var currentHeight = img.clientHeight + 'px';
 			var newHeight = (30 * oneVW) + 'px';
-			var img = document.querySelector('.header>.img-container');
+			
 			img.animate({height: [currentHeight, newHeight]},
 								 {
 								 	fill: 'forwards',
@@ -51,16 +57,14 @@ function navbarScroll (event) {
 								 });
 			window.scrollTo({
 				'behavior': 'smooth',
-				'left': 0,
-				'top': document.getElementById(href).offsetTop
+				'top': document.getElementById(href).offsetTop + newHeight
 			});
 		} else {
-			var smallHeader = document.getElementById('topHeader').clientHeight + 15 * oneVW;
+			var smallHeader = document.getElementById('topHeader').clientHeight + 10 * oneVW;
 
 			window.scrollTo({
 				'behavior': 'smooth',
-				'left': 0,
-				'top': document.getElementById(href).offsetTop + smallHeader
+				'top': document.getElementById(href).offsetTop - smallHeader
 			});
 		}
 	};
